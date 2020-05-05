84e71f4a368628a3d6b76090cc843fa0ebc0c8b4
junichi-fujita <fujita0909j@gmail.com>
2020-05-05 21:40:57 +0900

    [add] 権限移動のボタン作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resource :user
   
   resources :teams do
+    patch "transfer", on: :collection
     resources :assigns, only: %w(create destroy)
     resources :agendas, shallow: true do
       resources :articles do
